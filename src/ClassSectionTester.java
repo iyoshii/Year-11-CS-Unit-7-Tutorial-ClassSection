11bb1180a6861309b3a59d712990008efa3baf5b
yoshi <puggiedountsme@gmail.com>
2024-03-20 09:12:09 +0800

    Update 1.0

--- src/ClassSectionTester.java
+++ src/ClassSectionTester.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
-
+import static org.junit.jupiter.api.Assertions.*;
 public class ClassSectionTester {
 
     private Student student1;
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,66 @@
+import java.util.Objects;
+
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount = 1;
 
+    public Student(String firstName, String lastName, int age, int yearLevel){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        this.id = studentCount++;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public static int getStudentCount() {
+        return studentCount;
+    }
+
+    public boolean equals(Student other){
+        return (this.getFirstName().equals(other.getFirstName())) && (this.getLastName().equals(other.getLastName())) && (this.getAge()==other.getAge()) && (this.getYearLevel() == other.getYearLevel());
+    }
+    public String toString() {
+        return "Student{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "', age=" + age + ", yearLevel=" + yearLevel + "}";
+    }
 }
+
